5ca4190d3fa19e6fe15324c3ee2d359ac3a3da1b
Roland Becker <roland.becker@univ-pau.fr>
2020-04-06 20:49:37 +0200

    marche 1d

--- matrix.py
+++ matrix.py
@@ -9,8 +9,8 @@ def createMatrixDiff1d(grid):
     nx = grid.n[0]
     dx = grid.dx[0]
     offdiag = -np.ones(nx - 1)/dx/dx
-    left = offdiag
-    right = offdiag
+    left = offdiag.copy()
+    right = offdiag.copy()
     diag = 2*np.ones(nx)/dx/dx
     diag[0] /= 2
     diag[-1] /= 2
@@ -29,7 +29,7 @@ def createVectorDiff1d(grid, f, u):
     """
     x = grid.x()
     b = f(x)
-    print(f"x={x} b={b}")
+    # print(f"x={x} b={b}")
     if grid.bdrycond[0][0] == 'dirichlet': b[0] = u(x[0])
     if grid.bdrycond[0][1] == 'dirichlet': b[-1] = u(x[-1])
     return b
@@ -40,13 +40,15 @@ def createVectorDiff1d(grid, f, u):
 def test1d():
     import scipy.sparse.linalg as linalg
     import matplotlib.pyplot as plt
-    g = grid.Grid(n=[17], length=[[0,1]])
+    g = grid.Grid(n=[17], length=[[1,3]])
     g.bdrycond[0][0] = 'dirichlet'
     g.bdrycond[0][1] = 'dirichlet'
     A = createMatrixDiff1d(g)
     # print("A=", A.toarray())
-    f = np.vectorize(lambda x: np.pi**2*np.cos(np.pi*x))
     u = np.vectorize(lambda x: np.cos(np.pi*x))
+    f = np.vectorize(lambda x: np.pi**2*np.cos(np.pi*x))
+    # u = np.vectorize(lambda x: x*(1-x)+1)
+    # f = np.vectorize(lambda x: 2)
     b = createVectorDiff1d(g, f, u)
     uh = linalg.spsolve(A,b)
     x = g.x()

62c82aed9781afd8f286fe6a464f0ddeb3f7dd37
Roland Becker <roland.becker@univ-pau.fr>
2020-04-08 20:50:39 +0200

    2d dirichlet

--- grid.py
+++ grid.py
@@ -20,36 +20,26 @@ class Grid():
                 raise ValueError(f"Problem: all n must be even numbers. Given n={n}")
             self.dx[i] = (self.length[i][1]-self.length[i][0])/float(self.n[i]-1)
     def x(self):
-        if self.dim == 1:
-            return np.linspace(self.length[0][0], self.length[0][1], self.n[0])
-        elif self.dim == 2:
-            x = np.linspace(self.length[0][0], self.length[0][1], self.n[0])
-            y = np.linspace(self.length[1][0], self.length[1][1], self.n[1])
-            return np.meshgrid(x, y)
-        elif self.dim == 3:
-            x = np.linspace(self.length[0][0], self.length[0][1], self.n[0])
-            y = np.linspace(self.length[1][0], self.length[1][1], self.n[1])
-            z = np.linspace(self.length[2][0], self.length[2][1], self.n[2])
-            return np.meshgrid(x, y, z)
-        else:
-            raise ValueError(f"Problem: unable to plot in d={self.dim}")
+        x = []
+        for i in range(self.dim):
+            x.append(np.linspace(self.length[i][0], self.length[i][1], self.n[i]))
+        return x
+    def coord(self):
+        return np.meshgrid(*self.x(), indexing='ij')
 
     def plot(self, ax = None):
         if self.dim==1:
-            x = self.x()
+            x = self.coord()
             y = np.full_like(x, 1)
             if ax == None: ax = plt.gca()
-            ax.plot(x, y, 'x-')
+            ax.plot(x, y, 'rx-')
         elif self.dim == 2:
-            xx, yy = self.x()
+            xx, yy = self.coord()
             if ax == None: ax = plt.gca()
             ax.plot(xx, yy, marker='x', color='r', linestyle='none')
         elif self.dim == 3:
-            xx, yy, zz = self.x()
+            xx, yy, zz = self.coord()
             if ax == None: ax = plt.gca(projection='3d')
-            # if ax == None:
-            #     fig = plt.figure()
-            #     ax = fig.add_subplot(111, projection='3d')
             ax.scatter(xx, yy, zz, color='r')
         else:
             raise ValueError(f"Problem: unable to plot in d={self.dim}")
@@ -59,20 +49,30 @@ if __name__ == '__main__':
     grid1 = Grid(n=[5], length=[[1,3]])
     grid2 = Grid(n=[5, 7], length=[[1,3], [2,4]])
     grid3 = Grid(n=[5, 7, 3], length=[[1,3], [2,4], [0,2]])
-    print(grid1, grid2, grid3)
+    # print(grid1, grid2, grid3)
 
     # grid1.plot()
     # plt.show()
-    # grid2.plot()
-    # plt.show()
+    grid2.plot()
+    x,y = grid2.coord()
+    plt.plot(x[1,:], y[1,:], 'bo')
+    ufct = lambda x,y: x**2
+    u = ufct(x,y)
+    print("np.info(u)", np.info(u))
+    print("u[1,:]", u[1,:])
+    # u[1,:] = 10
+    print("u", u)
+    print("u.reshape(grid2.n[0], grid2.n[1])", u.reshape(grid2.n[0], grid2.n[1]))
+    cnt = plt.contourf(x, y, u)
+    plt.show()
     # grid3.plot()
     # plt.show()
 
-    fig = plt.figure()
-    ax = fig.add_subplot(3, 1, 1)
-    grid1.plot(ax)
-    ax = fig.add_subplot(3, 1, 2)
-    grid2.plot(ax)
-    ax = fig.add_subplot(3, 1, 3, projection='3d')
-    grid3.plot(ax)
-    plt.show()
+    # fig = plt.figure()
+    # ax = fig.add_subplot(3, 1, 1)
+    # grid1.plot(ax)
+    # ax = fig.add_subplot(3, 1, 2)
+    # grid2.plot(ax)
+    # ax = fig.add_subplot(3, 1, 3, projection='3d')
+    # grid3.plot(ax)
+    # plt.show()
--- matrix.py
+++ matrix.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.sparse as scsp
 import grid
+import simfempy.tools.analyticalsolution as anasol
 
 #-----------------------------------------------------------------#
 def createMatrixDiff1d(grid):
@@ -22,17 +23,79 @@ def createMatrixDiff1d(grid):
         left[-1] = 0
     A = scsp.diags(diagonals=(left, diag, right), offsets=[-1,0,1])
     return A.tocsr()
+#-----------------------------------------------------------------#
+def createMatrixDiff2d(grid):
+    """
+    """
+    nx, ny = grid.n[0], grid.n[1]
+    n = nx*ny
+    dx, dy = grid.dx[0], grid.dx[1]
+    diag = (2/dx/dx+2/dy/dy)*np.ones(shape=(nx,ny))
+    d0P = (-1/dy/dy)*np.ones(shape=(nx,ny))
+    d0M = (-1/dy/dy)*np.ones(shape=(nx,ny))
+    dP0 = (-1/dx/dx)*np.ones(shape=(nx,ny))
+    dM0 = (-1/dx/dx)*np.ones(shape=(nx,ny))
+
+    # mettre a zero ce qui sont dehors
+    d0M[:,0] = 0
+    d0P[:,-1] = 0
+    dM0[0,:] = 0
+    dP0[-1,:] = 0
+    # print("d0M", d0M)
+    # print("d0M", d0M.reshape(nx*ny))
+
+    if grid.bdrycond[0][0] == 'dirichlet':
+        diag[0,:] = 1
+        d0M[0,:] = d0P[0,:]= dM0[0,:]= dP0[0,:] = 0
+    if grid.bdrycond[0][1] == 'dirichlet':
+        diag[-1,:] = 1
+        d0M[-1, :] = d0P[-1, :] = dM0[-1, :] = dP0[-1, :] = 0
+    if grid.bdrycond[1][0] == 'dirichlet':
+        diag[:,0] = 1
+        d0M[:,0] = d0P[:,0] = dM0[:,0] = dP0[:,0] = 0
+    if grid.bdrycond[1][1] == 'dirichlet':
+        diag[:,-1] = 1
+        d0M[:,-1] = d0P[:,-1] = dM0[:,-1] = dP0[:,-1] = 0
+    diagonals = [diag.reshape(nx*ny)]
+    offsets = [0]
+    # print("d0M",d0M)
+    # print("shaores memory ?", np.shares_memory(d0M,d0Mrs))
+    diagonals.append(d0M.reshape(nx*ny)[1:])
+    offsets.append(-1)
+    diagonals.append(d0P.reshape(nx*ny)[:-1])
+    offsets.append(1)
+    diagonals.append(dM0.reshape(nx*ny)[nx:])
+    offsets.append(-nx)
+    diagonals.append(dP0.reshape(nx*ny)[:-nx])
+    offsets.append(nx)
+    A = scsp.diags(diagonals=diagonals, offsets=offsets)
+    # print("A=\n", A.toarray())
+    return A.tocsr()
 
 #-----------------------------------------------------------------#
-def createVectorDiff1d(grid, f, u):
+def createVectorDiff1d(grid, u):
     """
     """
-    x = grid.x()
-    b = f(x)
+    x, = grid.coord()
+    b = -u.xx(x)
     # print(f"x={x} b={b}")
     if grid.bdrycond[0][0] == 'dirichlet': b[0] = u(x[0])
     if grid.bdrycond[0][1] == 'dirichlet': b[-1] = u(x[-1])
     return b
+
+#-----------------------------------------------------------------#
+def createVectorDiff2d(grid, u):
+    """
+    """
+    x,y = grid.coord()
+    b = -u.xx(x,y) - u.yy(x,y)
+    print("b", b.shape)
+    # print(f"x={x} b={b}")
+    if grid.bdrycond[0][0] == 'dirichlet': b[0,:] = u(x[0,:],y[0,:])
+    if grid.bdrycond[0][1] == 'dirichlet': b[-1,:] = u(x[-1,:],y[-1,:])
+    if grid.bdrycond[1][0] == 'dirichlet': b[:,0] = u(x[:,0],y[:,0])
+    if grid.bdrycond[1][1] == 'dirichlet': b[:,-1] = u(x[:,-1],y[:,-1])
+    return b.ravel()
 
 
 #=================================================================#
@@ -45,15 +108,41 @@ def test1d():
     g.bdrycond[0][1] = 'dirichlet'
     A = createMatrixDiff1d(g)
     # print("A=", A.toarray())
-    u = np.vectorize(lambda x: np.cos(np.pi*x))
-    f = np.vectorize(lambda x: np.pi**2*np.cos(np.pi*x))
-    # u = np.vectorize(lambda x: x*(1-x)+1)
-    # f = np.vectorize(lambda x: 2)
-    b = createVectorDiff1d(g, f, u)
+    u = anasol.AnalyticalSolution('cos(pi*x)')
+    u = anasol.AnalyticalSolution('x*(1-x)+1')
+    b = createVectorDiff1d(g, u)
+    print("b=",b)
     uh = linalg.spsolve(A,b)
-    x = g.x()
+    x, = g.coord()
     plt.plot(x,uh, '-x', x, u(x))
     plt.show()
 
+
+def test2d():
+    import scipy.sparse.linalg as linalg
+    import matplotlib.pyplot as plt
+    g = grid.Grid(n=[15,15], length=[[0,1],[0,1]])
+    g.bdrycond[0][0] = 'dirichlet'
+    g.bdrycond[0][1] = 'dirichlet'
+    g.bdrycond[1][0] = 'dirichlet'
+    g.bdrycond[1][1] = 'dirichlet'
+    A = createMatrixDiff2d(g)
+    # print("A=", A.toarray())
+    # u = np.vectorize(lambda x,y: np.cos(np.pi*x)*np.cos(np.pi*y))
+    # f = np.vectorize(lambda x,y: 2*np.pi**2*np.cos(np.pi*x)*np.cos(np.pi*y))
+    # u = np.vectorize(lambda x,y: x*(1-x)+y*(1-y)+1)
+    # f = np.vectorize(lambda x,y: 4)
+    # u = anasol.AnalyticalSolution('cos(pi*x)*cos(pi*y)')
+    u = anasol.AnalyticalSolution('x*(1-x)+y*(1-y)+1')
+    b = createVectorDiff2d(g, u)
+    uh = linalg.spsolve(A,b)
+    x,y = g.coord()
+    # plt.plot(x,uh, '-x', x, u(x))
+    cnt = plt.contour(x, y, uh.reshape(g.n[0],g.n[1]))
+    plt.clabel(cnt, cnt.levels, inline=True, fmt='%.1f', fontsize=10)
+    plt.show()
+
 if __name__ == '__main__':
-    test1d()
+    # print("simfempy", simfempy.__version__)
+    # test1d()
+    test2d()
